6e3305657db800add5a1ce84b009c90af7b8fa4f
vadim <vaaadim.ermolin@mail.ru>
2023-07-03 22:13:10 +0300

    Изменение 4

--- app/src/main/java/com/example/mars/MainActivity.kt
+++ app/src/main/java/com/example/mars/MainActivity.kt
@@ -23,34 +23,4 @@ class MainActivity : AppCompatActivity() {
             startActivity(i)
         }
     }
-
-
-}
-package com.example.mars
-
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.view.View
-import com.example.mars.databinding.ActivityMainBinding
-
-class MainActivity : AppCompatActivity() {
-
-    lateinit var binding : ActivityMainBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.bt1.setOnClickListener {
-            val i = Intent(this, TestActivity1::class.java)
-            val j = binding.editTextText.text.toString()
-            i.putExtra("key", j)
-
-            startActivity(i)
-        }
-    }
-
-
 }
