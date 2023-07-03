3160f71399acd8d296a2bc35a4170cfac457ab01
vadim <vaaadim.ermolin@mail.ru>
2023-07-03 21:54:58 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mars/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.mars
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.mars.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bt1.setOnClickListener {
+            val i = Intent(this, TestActivity1::class.java)
+            val j = binding.editTextText.text.toString()
+            i.putExtra("key", j)
+
+            startActivity(i)
+        }
+    }
+
+
+}
